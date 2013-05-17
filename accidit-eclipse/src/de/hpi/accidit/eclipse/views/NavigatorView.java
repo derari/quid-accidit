3ada97a25aaf8ebc04a49100906f44e4e338b024
Sten Aechtner <sten.aechtner@student.hpi.uni-potsdam.de>
2013-05-17 14:27:59 +0200

    navigator view layout and event handlers

--- accidit-eclipse/src/de/hpi/accidit/eclipse/views/NavigatorView.java
+++ accidit-eclipse/src/de/hpi/accidit/eclipse/views/NavigatorView.java
@@ -1,64 +1,168 @@
 package de.hpi.accidit.eclipse.views;
 
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.part.ViewPart;
 
 public class NavigatorView extends ViewPart {
 
-	private static final String[] COLUMN_NAMES = {"#", "username", "message"};
-	private static final int[] COLUMN_WIDTHS = {30, 100, 200};
-
-	private TableViewer viewer;
-	private Text searchBox;
-
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "de.hpi.accidit.eclipse.views.NavigatorView";
+	
+	private Composite overComposite;
+	private Label intoLabel, overLabel;
+	private Label upLabel, leftLabel, rightLabel, downLabel;
+	private Label upLabel2, leftLabel2;
+	
 	public NavigatorView() { }
 
 	@Override
 	public void createPartControl(Composite parent) {
-		GridLayout layout = new GridLayout(1, true);
+		GridLayout layout = new GridLayout(3, false);
 		parent.setLayout(layout);
 		
-		searchBox = new Text(parent, SWT.SINGLE | SWT.SEARCH | SWT.ICON_SEARCH);
-		searchBox.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		searchBox.setText("vtipy");             
+		Label previousLabel = new Label(parent, SWT.NONE);
+		previousLabel.setText("Previous");
+		GridData layoutData = new GridData(SWT.LEFT, SWT.TOP, false, false);
+		layoutData.horizontalSpan = 2;
+		previousLabel.setLayoutData(layoutData);
+		
+		Composite intoComposite = new Group(parent, SWT.NONE);
+		RowLayout defaultLayout = new RowLayout();
+		intoComposite.setLayout(defaultLayout);
+		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		layoutData.verticalSpan = 2;
+		intoComposite.setLayoutData(layoutData);
+		intoLabel = new Label(intoComposite, SWT.NONE);
+		intoLabel.setText("Into");
 
-		viewer = new TableViewer(parent, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
-		createColumns(viewer);
-//		viewer.se.setLabelProvidetContentProvider(new MessageTimelineContentProvider());
-//		viewerr(new MessageTimelineLabelProvider());
-		// the input for the content provider
-		viewer.setInput(searchBox);             
-		viewer.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
+		Label outLabel = new Label(parent, SWT.NONE);
+		outLabel.setText("Out");
+		layoutData = new GridData(SWT.LEFT, SWT.BOTTOM, false, false);
+		outLabel.setLayoutData(layoutData);
+		
+		Label upArrow = new Label(parent, SWT.NONE);
+		upArrow.setText("A");
+		layoutData = new GridData(SWT.CENTER, SWT.BOTTOM, false, false);
+		upArrow.setLayoutData(layoutData);
+		
+		Label leftArrow = new Label(parent, SWT.NONE);
+		leftArrow.setText("<");
+		layoutData = new GridData(SWT.RIGHT, SWT.CENTER, false, false);
+		leftArrow.setLayoutData(layoutData);
+		
+		Label placeHolder = new Label(parent, SWT.NONE);
+		
+		Label rightArrow = new Label(parent, SWT.NONE);
+		rightArrow.setText(">");
+		layoutData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
+		rightArrow.setLayoutData(layoutData);
+		
+		Label resultLabel = new Label(parent, SWT.NONE);
+		resultLabel.setText("Result");
+		layoutData = new GridData(SWT.LEFT, SWT.TOP, false, false);
+		resultLabel.setLayoutData(layoutData);
+		
+		Label downArrow = new Label(parent, SWT.NONE);
+		downArrow.setText("V");
+		layoutData = new GridData(SWT.CENTER, SWT.TOP, false, false);
+		downArrow.setLayoutData(layoutData);
+		
+		overComposite = new Group(parent, SWT.NONE);
+		overComposite.setLayout(defaultLayout);
+		layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		layoutData.verticalSpan = 2;
+		overComposite.setLayoutData(layoutData);
+		overLabel = new Label(overComposite, SWT.NONE);
+		overLabel.setText("Over");
+				
+		Label nextLabel = new Label(parent, SWT.NONE);
+		nextLabel.setText("Next");
+		layoutData = new GridData(SWT.LEFT, SWT.BOTTOM, false, false);
+		layoutData.horizontalSpan = 2;
+		nextLabel.setLayoutData(layoutData);
+		
+		parent.pack();
+				
+		Display.getDefault().addFilter(SWT.KeyDown, new Listener(){
 
-		// tuning of the underlying table               
-		viewer.getTable().setLinesVisible(true);                
-		viewer.getTable().setHeaderVisible(true);
-
-		// make selection available to others
-		getSite().setSelectionProvider(viewer);
+			@Override
+			public void handleEvent(org.eclipse.swt.widgets.Event event) {
+				if (event.type != SWT.KeyDown)  return;
+				if(!(getSite().getPage().getActivePart() instanceof NavigatorView)) return; // check if navigator view has focus
+				
+				switch(event.keyCode) {
+				case SWT.ARROW_DOWN: System.out.println("arrow down!"); break;
+				default: break;
+				}
+			}	
+		});
+		
+//		hookListener(upComposite);
+//		hookListener(upLabel);
+//		hookListener(upLabel2);
+//		
+//		hookListener(leftLabel2);
+//		
+//		hookListener(downComposite);
+	}
+	
+	private void hookListener(final Control control) {
+		control.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseUp(MouseEvent e) {
+				System.out.println("Event! - " + control);
+			}
+			
+			@Override
+			public void mouseDown(MouseEvent e) {
+				return;
+			}
+			
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				return;
+			}
+		});
 	}
 
-	private void createColumns(TableViewer tableViewer) {
-		for (int i = 0; i < COLUMN_NAMES.length; i++) {
-			TableViewerColumn tvColumn = new TableViewerColumn(tableViewer, SWT.NULL);
-			TableColumn column = tvColumn.getColumn();
-			column.setWidth(COLUMN_WIDTHS[i]);
-			column.setText(COLUMN_NAMES[i]);
-			// I can also here register separated cell providers via calling TableViewerColumn.setLabelProvider
-			// - @see CellLabelProvider or StyledCellLabelProvider                  
-		}               
+	private void createImage(Composite parent) {
+		Display compositeDisplay = parent.getDisplay();
+		Image image = new Image(compositeDisplay, 16, 16);
+		Color color = compositeDisplay.getSystemColor(SWT.COLOR_RED);
+		GC gc = new GC(image);
+		gc.setBackground(color);
+		gc.fillRectangle(image.getBounds());
+		gc.dispose();
+		
+		CLabel label = new CLabel(parent, SWT.BORDER);
+		Rectangle clientArea = parent.getClientArea();
+		label.setLocation(clientArea.x, clientArea.y);
+		label.setImage(image);
+		label.setText("the text");
+		label.pack();
 	}
 
 	@Override
 	public void setFocus() {
-		this.searchBox.setFocus();
+//		upComposite.setFocus();
 	}
-
 }
