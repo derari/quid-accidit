aa9642915b7c4bfddabd8a03329ef6f059e353f0
Arian Treffer <arian.treffer@hpi.uni-potsdam.de>
2013-10-11 15:46:07 +0200

    rename 1/2

--- accidit-eclipse/src/de/hpi/accidit/eclipse/handlers/AddBreakpointHandler.java
+++ accidit-eclipse/src/de/hpi/accidit/eclipse/handlers/AddBreakpointHandler.java
@@ -5,14 +5,14 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import de.hpi.accidit.eclipse.views.BreakpointsView;
+import de.hpi.accidit.eclipse.views.BreakpointsViewx;
 
 public class AddBreakpointHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		BreakpointsView breakpointsView = 
-				(BreakpointsView) HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().findView(BreakpointsView.ID);
+		BreakpointsViewx breakpointsView = 
+				(BreakpointsViewx) HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().findView(BreakpointsViewx.ID);
 		breakpointsView.addBreakpointLine();
 		return null;
 	}
